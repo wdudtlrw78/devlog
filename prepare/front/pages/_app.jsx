678bfd6ac08bd7653c057789f25406925f3142c7
momo <wdudtlrw78@gmail.com>
2021-06-03 23:02:20 +0900

    Add: emotion globalStlye and icon

---
+++ prepare/front/pages/_app.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Global, css } from '@emotion/react';
+
+library.add(fab, faBars);
+
+export const globalStyles = (
+  <Global
+    styles={css`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: Noto Sans KR, sans-serif;
+          line-height: 1.6;
+          font-size: 14px;
+        }
+        
+        * {
+          box-sizing: border-box;
+        }
+        
+        a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+        
+        .menu-button {
+          color: #E96900;
+        }
+        
+        a:hover {
+          text-decoration: underline;
+        }
+        
+        img {
+          max-width: 100%;
+          display: block;
+        `}
+  />
+);
+
+const MolyMath = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>MolyMath</title>
+      </Head>
+      {globalStyles}
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+MolyMath.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default MolyMath;
--- prepare/front/pages/index.jsx
+++ prepare/front/pages/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
-  return <div>HOME</div>;
+  return <AppLayout>HOME</AppLayout>;
 };
 
 export default Home;
