2b43dd6ea1d07f244d8e3d08fefcc49d3730068b
momo <wdudtlrw78@gmail.com>
2021-06-18 00:51:42 +0900

    Add: dummyPost

--- prepare/front/pages/index.jsx
+++ prepare/front/pages/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import AppLayout from '../components/AppLayout';
 import PostCard from '../components/PostCard';
 
 const Home = () => {
+  const { mainPosts } = useSelector((state) => state.post);
   return (
     <AppLayout>
-      <PostCard />
+      {mainPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </AppLayout>
   );
 };
--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,9 +1,53 @@
 const initialState = {
-  mainPosts: [],
+  mainPosts: [
+    {
+      id: 1,
+      header: '이벤트 위임',
+      body: 'body',
+      description: 'description',
+      category: 'JavaScript',
+      Comments: [
+        {
+          User: {
+            nickname: 'MoMo',
+          },
+          content: 'test1',
+        },
+        {
+          noUserSignup: {
+            email: 'wdudtlrw@gmail.com',
+            nickname: '익명',
+            password: '1234567890',
+          },
+          content: 'test2',
+        },
+      ],
+    },
+  ],
+
+  postAdded: false,
+};
+
+export const ADD_POST = 'ADD_POST';
+
+export const addPost = {
+  type: ADD_POST,
+};
+
+const dummyPost = {
+  id: 2,
+  content: 'this is dummyPost',
+  Comments: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [...state.mainPosts, dummyPost],
+        postAdded: true,
+      };
     default:
       return state;
   }
