eaa667ba28c684505c131077226aed3861cd9393
momo <wdudtlrw78@gmail.com>
2021-06-17 22:01:01 +0900

    Add: userprofile title

--- prepare/front/components/UserProfile/index.jsx
+++ prepare/front/components/UserProfile/index.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Link from 'next/link';
 import gravatar from 'gravatar';
+import { useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { Container, LogoutButton, SettingButton } from './styles';
+import { logoutAction } from '../../reducers/user';
 
 const UserProfile = () => {
+  const dispatch = useDispatch();
+
+  const onLogout = useCallback(() => {
+    dispatch(logoutAction());
+  }, []);
+
   return (
     <Container>
       <img src={gravatar.url('모모', { s: '24px', d: 'retro' })} alt="모모" />
       <span className="profile-nickname">모모</span>
 
-      <SettingButton>
+      <SettingButton title="설정">
         <Link href="/settings">
           <a>
             <div className="profile-setting">
@@ -20,7 +28,7 @@ const UserProfile = () => {
           </a>
         </Link>
       </SettingButton>
-      <LogoutButton>
+      <LogoutButton onClick={onLogout} title="로그아웃">
         <div className="profile-logout">
           <FontAwesomeIcon icon={faSignOutAlt} size="2x" />
         </div>
