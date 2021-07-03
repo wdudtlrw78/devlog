9122693f3574857fbae1a162fec6c98125fe85ab
momo <wdudtlrw78@gmail.com>
2021-07-04 04:07:30 +0900

    Remove: 404 page

--- prepare/front/pages/guestbook.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppLayout from '../components/AppLayout';
-
-const Guestbook = () => {
-  return <AppLayout>Guestbook</AppLayout>;
-};
-
-export default Guestbook;
